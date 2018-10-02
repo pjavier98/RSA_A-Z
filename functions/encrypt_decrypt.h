48092875ffae4419e56790c8ecd4e6ff0c1f4080
joaovictor <joao-victorferro@hotmail.com>
2018-10-01 23:02:23 -0300

    falta completar os comentarios

--- functions/encrypt_decrypt.h
+++ functions/encrypt_decrypt.h
@@ -1,27 +1,35 @@
-#ifndef encypt_decrypt
-#define encypt_decrypt
+# Ifndef encypt_decrypt
+# define  encypt_decrypt
 
 
-#include "../header/header.h"
+# include  " ../header/header.h "
 
-lli exponentiation(lli num, lli exp, lli mod);
+lli exponentiation (lli num, lli exp, lli mod);
 /*
  *
  */
 
-lli fermat(lli number, lli expoente, lli mod);
-/*
- *
+lli fermat (número lli, lli expoente, lli mod);
+/* Recebe o resultado da função exponentiation, transformando para long long int; 
  */
 
-void txt_to_encrypt();
-/*
- *
+void  txt_to_encrypt ();
+/* Cria um array com o alfabeto e com o espaço incluido de 0-27;
+ * Abre o arquivo teste.txt, na qual tem a mensagem a ser encriptografada;
+ * Recebe o key1 e o key2 do usuario para encriptografa a mensagem;
+ * Depois ler caracter por caracter no arquivo teste.tct 
+ * Onde ira pega a posição do caracter a ser encriptado, para passar a função fermat;
+ * Também o arquivo criptografado.txt é aberto, que guardará o resultado da função fermat;
  */
 
-void txt_to_decrypt();
-/*
- *
+void  txt_to_decrypt ();
+/* Cria um array com o alfabeto e com o espaço incluido de 0-27;
+ * cria um while para ver o tamanho do arquivo;
+ * Abre o arquivo criptografado.txt, onde tem a mensagem criptografada;
+ * E também o arquivo key_private/key_private.txt e key_public/key_public.txt, 
+ * onde tem as chaves publicas e privadas para descriptografa;
+ * Passa um loop, lendo os numeros encriptofrafado para a função fermat, na qual recebe um valor que ia ser buscado no array,
+ * logo printando esse char no arquivo;
  */
 
-#endif
+# endif
--- functions/generate_keys.h
+++ functions/generate_keys.h
@@ -1,46 +1,46 @@
-#ifndef generate_keys
-#define generate_keys
+# ifndef generate_keys
+# define  generate_keys
 
-#include "../header/header.h"
+# include  " ../header/header.h "
 
-void keys();
-/*
- * 
+void  keys ();
+/* Digita um numero primo p e depois um numero primo q, e fica verificando se esse numeros sao primos,;
+ * Chamando a funcao totiente, na qual retorna o resultado da multiplicacao dos primos -1;
+ * O usuario ira digita o valor de um coprimo ao resultado da totiente, e depois o valor e verificado ate esta certo, ou seja a segunda chave publica;
+ * Chama a funcao public_key, onde ira retorna o valor da segunda chave publica a multiplicao dos primos;
+ * Chama a funcao inverso_modular, na qual ira retorna o valor da 3 chave privada;
+ * E aberto o arquivo "key_public/key_public.txt", onde ira guarda as chaves publicas;
+ * O arquivo "key_private/key_private.txt", ficara guardado as chaves privadas;
  */
 
-void mdc(lli num_1, lli num_2, lli *cont);
-/*
- * 
+void  mdc (lli num_1, lli num_2, lli * cont);
+/* mdc de euclides, ate que o resultado do num2 seja = 0 para parar, e tem um cont que fica somando +1 a cada loop que num seja diferente de 0;
  */
 
-short primo(lli num);
-/*
- *
+ short primo (lli num);
+/* Na qual verifica se o numero e primo ou nao;
  */
 
-short coprimo(lli num_1, lli num_2);
-/*
- *
+ short coprimo (lli num_1, lli num_2);
+/* Verifica se o numero e coprimo ou nao;
  */
 
-lli totiente(lli key_1, lli key_2); 
-/*
- * 
+lli totiente (lli key_1, lli key_2);
+/* Multiplicacao dos primos -1 ((q-1) * (p-1)), e retorna esse resultado;
  */
 
-lli public_key(lli p, lli q); 
-/*
- * 
+lli public_key (lli p, lli q);
+/* Multiplicacao dos 2 primos;
  */
 
-lli linear_comb(lli num_1, lli num_2, lli coef_s, lli cont);
-/*
- * 
+lli linear_comb (lli num_1, lli num_2, lli coef_s, lli cont);
+/* primeiro verifica se o numero e divisilve por ele ou nao (cont == 1), ou o contador do mds e 2, para receber o coeficiente;
+ * caso o numero tenha + de um divisor  (falta completar)
  */
 
-lli inverso_modular(lli expoente, lli res_totiente);
-/*
- * 
+lli inverso_modular (lli expoente, lli res_totiente);
+/* Chama afucao mds, que ira por meio de ponteiro recebe o cont (quantidade de loops da funcao mds);
+ * Depois executa a funcao liner_comb, que ira retorna o resultado do coeficente (falta completar); 
  */
 
-#endif
+# endif
